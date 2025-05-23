e1271b87a4499095e902f6d6bad04774544ccfc1
samuele301197 <parisisamu32@gmail.com>
2025-05-23 21:09:37 +0200

    feat:bonus

---
+++ routers/data.js
@@ -0,0 +1,41 @@
+const birrePreferite = [
+  {
+    titolo: "Brewdog, Punk Ipa",
+    id: 1,
+    tipologia: "Indian Pale Ale",
+    immagine: "img/punk-ipa.webp",
+    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
+  },
+  {
+    titolo: "Birrificio Baladin, Birra Nazionale ",
+    id: 2,
+    tipologia: "Golden Ale, Blonde ALe",
+    immagine: "img/birra-nazionale.jpg",
+    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
+  },
+  {
+    titolo: "Dubuisson, Bush Blonde ",
+    id: 3,
+    tipologia: "Belgian Strong ALe",
+    immagine: "img/bush-blonde.webp",
+    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
+  },
+  {
+    titolo: "Birra Antoniana, Scudata",
+    id: 4,
+    tipologia: "Lager, Helles",
+    immagine: "img/scudata.png",
+    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
+  },
+  {
+    titolo: "Birrificio Baladin, L'IPA ",
+    id: 5,
+    tipologia: "Indian Pale Ale",
+    immagine: "img/baladin-ipa.jpg",
+    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
+  }
+];
+
+
+
+export { birrePreferite };
--- routers/file/birre.js
+++ routers/file/birre.js
@@ -1,44 +1,8 @@
 import express from "express"
+import { birrePreferite } from "../data.js";
 const router = express.Router();
 
-const birrePreferite = [
-  {
-    titolo: "Brewdog, Punk Ipa",
-    id: 1,
-    tipologia: "Indian Pale Ale",
-    immagine: "img/punk-ipa.webp",
-    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
-  },
-  {
-    titolo: "Birrificio Baladin, Birra Nazionale ",
-    id: 2,
-    tipologia: "Golden Ale, Blonde ALe",
-    immagine: "img/birra-nazionale.jpg",
-    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
-  },
-  {
-    titolo: "Dubuisson, Bush Blonde ",
-    id: 3,
-    tipologia: "Belgian Strong ALe",
-    immagine: "img/bush-blonde.webp",
-    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
-  },
-  {
-    titolo: "Birra Antoniana, Scudata",
-    id: 4,
-    tipologia: "Lager, Helles",
-    immagine: "img/scudata.png",
-    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
-  },
-  {
-    titolo: "Birrificio Baladin, L'IPA ",
-    id: 5,
-    tipologia: "Indian Pale Ale",
-    immagine: "img/baladin-ipa.jpg",
-    tags: ["Mastri Birrai", "Alta Fermentazione", "Birre Italiane"]
-  }
-];
-
+// ROTTE
 
 router.get("/", (req, res) => {
     res.json ({
@@ -50,8 +14,7 @@ router.get("/:id", (req, res) => {
     const birreID = parseInt(req.params.id);
     const birra = birrePreferite.find((curElem) => curElem.id === birreID);
     res.json({
-        data: `mostro un ID ${birreID}`,
-        birra: birra
+        data: `mostro un ID ${birreID}`, birra
     });
 });
 
